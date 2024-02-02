60237fec4b187fbf7d8694a8bab4a8542adcc8fc
Korku <agbenguklenam31@gmail.com>
2024-02-02 10:37:41 +0000

    added new files

---
+++ arithmeticOperators.js
@@ -0,0 +1,7 @@
+const num1=2;
+const num2=4;
+const sum=num1+num2;//addition
+const difference=num1-num2;//subtraction
+const division=num1/num2//division
+const multile=num1*num2//multiplication
+const mod=num1%num2//modulus
---
+++ variables.js
@@ -0,0 +1,8 @@
+const statement="Not Hello World";//variable declaration
+let changeableStatement ="Not Hello World";//camelcase; snake_case
+var variable="old way of doing things";
+//multiple declaration
+let variable1,variable2,variable3;
+variable1=5;
+changeableStatement="new value";
+console.log(variable1);
